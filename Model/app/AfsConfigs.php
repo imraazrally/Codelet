fed14672c46adbadc299f9f71b9a40979c98985f
Imraaz Rally <raisoft2020@gmail.com>
2015-09-30 00:30:26 -0400

    Model

--- Model/app/AfsConfigs.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define("VIEW_HOME_URL", "https://web.njit.edu/~vb97/Codelet/");
-	define("CONTROLLER_HOME_URL", "https://web.njit.edu/~hmd9/Codelet/" );
-	define("MODEL_HOME_URL", "https://web.njit.edu/~ir29/Codelet/" );
-	define("CONTROLLER_ROUTER_URL", CONTROLLER_HOME_URL."Router.php");
-	define("MODEL_ROUTER_URL", MODEL_HOME_URL."Router.php");
-?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+	include ("app/Dispatcher.php");
+	Dispatcher::forward("https://web.njit.edu/~vb97/Codelet/");
+?>
